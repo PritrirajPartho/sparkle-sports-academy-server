d377443e946ece0ab1e4079b025ab3af4b1af40d
PritrirajPartho <dapartho5616@gmail.com>
2023-06-09 14:44:19 +0600

    insructor-verify

--- index.js
+++ index.js
@@ -68,6 +68,31 @@ async function run() {
     // }
 
     // users relatd apis.......
+    
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === 'instructor' }
+      res.send(result);
+    })
+
 
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
