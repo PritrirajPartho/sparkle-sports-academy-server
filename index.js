41362f12a0337a53de977fa256aea2a2621ddd48
PritrirajPartho <dapartho5616@gmail.com>
2023-06-09 13:32:32 +0600

    jwt-worked

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+// const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 //middleware..........
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,6 +49,23 @@ async function run() {
     const bookedCollection = client.db("campDb").collection("booked");
     const usersCollection = client.db("campDb").collection("users");
     
+
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        res.send({ token })
+    })
+
+    // // use verifyJWT before using verifyAdmin
+    // const verifyAdmin = async (req, res, next) => {
+    //     const email = req.decoded.email;
+    //     const query = { email: email }
+    //     const user = await usersCollection.findOne(query);
+    //     if (user?.role !== 'admin') {
+    //       return res.status(403).send({ error: true, message: 'forbidden message' });
+    //     }
+    //     next();
+    // }
 
     // users relatd apis.......
 
@@ -69,7 +103,7 @@ async function run() {
 
     })
 
-    app.patch('/users/instructor/id', async (req, res) => {
+    app.patch('/users/instructor/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const filter = { _id: new ObjectId(id) };
@@ -91,16 +125,18 @@ async function run() {
     })
 
    //booked
-    app.get('/bookeds', async (req, res) => {
+    app.get('/bookeds', verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
       }
-      // const decodedEmail = req.decoded.email;
-      // if (email !== decodedEmail) {
-      //   return res.status(403).send({ error: true, message: 'forbidden access' })
-      // }
+
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
+      }
+      
       const query = { email: email };
       const result = await bookedCollection.find(query).toArray();
       res.send(result);
